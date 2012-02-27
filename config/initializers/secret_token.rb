e60d58eb8f4342c7675f76ebea036fe80f858b78
Tobias Hetzel <tchetzel@gmail.com>
2012-02-27 01:55:07 -0800

    mein kleiner shop erster commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NepaliShop::Application.config.secret_token = 'c4cd27770b88db3ebd1a405226c0b1778af6bdcb662e8a6eb790df40268af251da4cf1bc2b3d8752c2d5d846abea35490791fc7b86d2ee26f3ab39e38e3cefe6'
